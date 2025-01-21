c87bb86350568b8d67d4582db3b55edfc8578b3b
Guilherme-Vilani <gui.vilani79@gmail.com>
2025-01-21 17:31:16 -0400

    esqueleto da calculadora feito em html

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<!-- <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora</title>
+</head> -->
+<head>
+    <meta charset="UTF-8">
+    <title>Calculadora PHP</title>
+    <link rel="stylesheet" href="style.css"> <!-- Substitua "style.css" pelo caminho correto do seu arquivo CSS -->
 </head>
 <body>
-    <form method="post" action=''>
+    <!-- <form method="post" action=''>
         
         <label for="valor1">Valor 1:</label>
         <br>
@@ -20,8 +25,34 @@
         
         <input type="submit" name="subtrair" class="button" value="Subtrair" />
         <br><br>
-    </form>
+    </form> -->
 
+    <div class="container">
+        <div class="box">
+            <div class="visor">
+                <a class="texto-visor">0</a>
+            </div>
+            <!-- <div class="container-botoes-numeros"> -->
+                <div class="botoes-numero-linha">
+                    <a class="texto-numero-botoes-linha">9</a>
+                    <a class="texto-numero-botoes-linha">8</a>
+                    <a class="texto-numero-botoes-linha">7</a>
+                </div>
+                <div class="botoes-numero-linha">
+                    <a class="texto-numero-botoes-linha">6</a>
+                    <a class="texto-numero-botoes-linha">5</a>
+                    <a class="texto-numero-botoes-linha">4</a>
+                </div>
+                <div class="botoes-numero-linha">
+                    <a class="texto-numero-botoes-linha">3</a>
+                    <a class="texto-numero-botoes-linha">2</a>
+                    <a class="texto-numero-botoes-linha">1</a>
+                </div>
+            <!-- </div> -->
+        </div>
+    </div>
+
+    <!-- INICIO CODIGO PHP -->
     <?php
         if(array_key_exists('calcular', $_POST)){
             calcular();
